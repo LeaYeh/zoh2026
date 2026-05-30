93bf1a26dfe578a0683befbe99643e14f7a326c1
Lea Yeh <lea.yeh.ml@gmail.com>
2026-05-30 08:04:20 +0200

    chore: remove track3 configs, scripts, skills, and ADRs

--- scripts/run_backtest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SYMBOL=${1:-"AAPL"}
-MODEL=${2:-"chronos"}
-
-echo "Running backtest: symbol=$SYMBOL model=$MODEL"
-python -m src.evaluation.backtesting --symbol "$SYMBOL" --model "$MODEL"
